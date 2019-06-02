3fb66fe91d7720be24724078d23f64c398910f25
Tobias Weiss <tweiss@student.tgm.ac.at>
2019-06-02 21:26:47 +0200

    Main liest nun von einer Datei ein (Hardcoded)

--- weiss/Main.java
+++ weiss/Main.java
@@ -1,12 +1,24 @@
 package weiss;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import weiss.tokenizer.Token;
 import weiss.tokenizer.Tokenizer;
 
 public class Main {
-	public static void main (String[] args) {
-		String s = "Name;Klasse;Note\nTEXT(2,50);TEXT(2,7);GANZZAHL(1,5)\n\"Tobias Weiss\";;2";
-		Tokenizer tn = new Tokenizer(s);
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = Files.newBufferedReader(Paths.get("/home/tobias/java-school/Konvertertool/test.csvp"));
+		
+		StringBuilder s = new StringBuilder();
+		
+		while(br.ready()) {
+			s.append((char) br.read());
+		}
+		
+		Tokenizer tn = new Tokenizer(s.toString());
 		
 		while(tn.hasToken()) {
 			Token curr = tn.getToken();
